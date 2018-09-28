3799a664220cf79ddd897a2c5c03ef163ccb5fac
DESKTOP-AM8FIS3\admin <tung.nguyen.thac@ntq-solution.com.vn>
2018-09-28 18:01:58 +0700

    done login

--- App.js
+++ App.js
@@ -2,31 +2,33 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './components/LoginScreen'
 import SlashScreen from './components/SlashScreen'
+import * as firebase from 'firebase';
 
- class Main extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state={
-        currentScreen: 'SlashScreen'
-      };
-      setTimeout(() => {
-          this.setState({currentScreen : 'Login'})
-      },10000)
-  }
-    render() {
-        const {currentScreen} = this.state;
-        let Main = currentScreen === 'SlashScreen' ? <SlashScreen/> : <LoginScreen/>;
-        return (
-           Main
-        );
-    }
-}
-
+//  class Main extends React.Component {
+//   constructor(props) {
+//       super(props);
+//       this.state={
+//         currentScreen: 'SlashScreen'
+//       };
+//       setTimeout(() => {
+//           this.setState({currentScreen : 'Login'})
+//       },2000)
+//   }
+//     render() {
+//         const {currentScreen} = this.state;
+//         let Main = currentScreen === 'SlashScreen' ? <SlashScreen/> : <LoginScreen/>;
+//         return (
+//            Main
+//         );
+//     }
+// }
+//
 
 export default class App extends React.Component {
   render() {
     return (
-     <Main/>
+     //<Main/>
+        <LoginScreen/>
     );
   }
 }
--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -2,19 +2,114 @@ import * as React from 'react';
 import {
     StyleSheet, Text, View, Image,
     TouchableWithoutFeedback, StatusBar,
-    TextInput, SafeAreaView, Keyboard, TouchableOpacity,
+    TextInput, SafeAreaView, Keyboard, TouchableOpacity, Alert,
     KeyboardAvoidingView
 } from 'react-native'
+import * as firebase from "firebase";
+import Expo from 'expo';
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDmSEQQw617T8htlon4Nt6_uvbq_emPoog",
+    authDomain: "travel-reactnative.firebaseapp.com",
+    databaseURL: "https://travel-reactnative.firebaseio.com",
+    projectId: "travel-reactnative",
+    storageBucket: "travel-reactnative.appspot.com",
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 export default class LoginScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            pass: ''
+        };
+    }
+
+    componentDidMount() {
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user != null) {
+                //console.log(user)
+            }
+        })
+    }
+
+    onSingUp = (email, pass) => {
+        try {
+            if (pass.length < 6) {
+                alert('Please enter atleast 6 characters')
+            }
+            firebase.auth().createUserWithEmailAndPassword(email, pass).then((user) => {
+                console.log(user);
+            })
+        } catch (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage)
+        }
+    };
+
+    async onSignFb() {
+        const {type, token} = await Expo.Facebook.logInWithReadPermissionsAsync('260015801320288', {
+            permissions: ["email", "public_profile", "user_friends"],
+        });
+        if (type === 'success') {
+            const credential = firebase.auth.FacebookAuthProvider.credential(token);
+
+            firebase.auth().signInAndRetrieveDataWithCredential(credential)
+                .then(function (userCredential) {
+                    console.log(userCredential.additionalUserInfo);
+                });
+        }
+    }
+
+    signInGoogle = async () => {
+        try {
+            const result = await Expo.Google.logInAsync({
+                androidClientId: '344842523937-fl2jonaaide98d550u1tvvghg41h8716.apps.googleusercontent.com',
+                scopes: ['profile', 'email'],
+            });
+            if (result.type === 'success') {
+                console.log(result) ;
+                const credential = firebase.auth.GoogleAuthProvider.credential(result.idToken);
+                console.log(credential);
+
+                firebase.auth().signInAndRetrieveDataWithCredential(credential)
+                    .then(function (userCredential) {
+                        console.log(userCredential);
+                    });
+            } else {
+                console.log('cancel ')
+            }
+        } catch(e) {
+            console.log('Errrrrrr')
+        }
+    };
+
+    onSingIn = (email, pass) => {
+        firebase.auth().signInWithEmailAndPassword(email, pass).then((user) => {
+            console.log(user);
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage);
+            // ...
+        });
+    };
+
     render() {
         return (
-            <SafeAreaView style={styles.container}>
-                <StatusBar barStyle="light-content" />
-                <KeyboardAvoidingView behavior='padding' style={styles.container}>
-                    <TouchableWithoutFeedback style={styles.container}
-                                              onPress={Keyboard.dismiss}>
-                        <View style={styles.logoContainer}>
-                            <View style={styles.logoContainer}>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgb(32, 53, 70)'}}>
+                <KeyboardAvoidingView behavior='padding'>
+                    <StatusBar barStyle="light-content"/>
+                    <View>
+                        <View style={styles.infoContainer}>
+                            <View>
                                 <Image style={styles.logo}
                                        source={require('../images/logo.png')}>
                                 </Image>
@@ -28,62 +123,71 @@ export default class LoginScreen extends React.Component {
                                            returnKeyType='next'
                                            autoCorrect={false}
                                            underlineColorAndroid="transparent"
-                                           onSubmitEditing={()=> this.refs.txtPassword.focus()}
+                                           onChangeText={(email) => this.setState({email})}
                                 />
                                 <TextInput style={styles.input}
                                            placeholder="Enter password"
                                            placeholderTextColor='rgba(255,255,255,0.8)'
+                                           secureTextEntry={true}
                                            returnKeyType='go'
-                                           secureTextEntry
                                            autoCorrect={false}
                                            underlineColorAndroid="transparent"
                                            ref={"txtPassword"}
+                                           onChangeText={(pass) => this.setState({pass})}
                                 />
-                                <TouchableOpacity style={styles.buttonContainer}>
-                                    <Text style={styles.buttonText}>SIGN IN</Text>
+                                <TouchableOpacity style={styles.buttonContainer} onPress={() => {
+                                    this.onSingIn(this.state.email, this.state.pass)
+                                }}>
+                                    <Text style={styles.buttonText}>Sign in with email</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={[styles.buttonContainer, {backgroundColor: 'red'}]}
+                                                  onPress={() => {
+                                                      this.signInGoogle()
+                                                  }}>
+                                    <Text style={[styles.buttonText, {color: 'white'}]}>Sign up with google</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={[styles.buttonContainer, {backgroundColor: 'blue'}]}
+                                                  onPress={() => {
+                                                      this.onSignFb()
+                                                  }}>
+                                    <Text style={[styles.buttonText, {color: 'white'}]}>Sign in with FaceBook</Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
-                    </TouchableWithoutFeedback>
+                    </View>
                 </KeyboardAvoidingView>
-            </SafeAreaView>
+                <Text style={{marginTop: 10, color: '#fff', textDecorationLine: 'underline'}} onPress={() => {
+                    this.onSingUp(this.state.email, this.state.pass)
+                }}>You do not have an account</Text>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: 'rgb(32, 53, 70)',
-        flexDirection: 'column',
-    },
-    logoContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        // flex: 1
     },
     logo: {
+        justifyContent: 'center',
         width: 128,
         height: 56,
+        marginBottom: 20
     },
     title: {
         color: '#f7c744',
         fontSize: 18,
         textAlign: 'center',
-        marginTop: 5,
+        marginVertical: 15,
         opacity: 0.9
     },
     infoContainer: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
-        height: 200,
-        padding: 20,
-        marginBottom: 20
-        // backgroundColor: 'red'
+        alignItems: 'center'
     },
     input: {
+        width: 300,
         height: 40,
         backgroundColor: 'rgba(255,255,255,0.2)',
         color: '#FFF',
@@ -91,12 +195,15 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10
     },
     buttonContainer: {
+        marginTop: 10,
         backgroundColor: '#f7c744',
-        paddingVertical: 15
+        paddingHorizontal: 55,
+        paddingVertical: 10,
+        borderRadius: 5
     },
     buttonText: {
         textAlign: 'center',
-        color :'rgb(32, 53, 70)',
+        color: 'rgb(32, 53, 70)',
         fontWeight: 'bold',
         fontSize: 18
     }
